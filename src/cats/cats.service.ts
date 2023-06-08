8012cca288b981ca1895b0dbde1635404c99e8ec
jack <jrfisch95@gmail.com>
2023-06-08 16:09:13 -0500

    add config interface and database interface used for accessing whole nested custom configuration objects by using the interface as a type hint with the .get() method, e.g. this.configService.get<DatabaseConfig>('database')

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DatabaseConfig } from 'src/config/configuration';
 
 @Injectable()
 export class CatsService {
@@ -11,6 +12,15 @@ export class CatsService {
     console.log(this.configService.getOrThrow<string>('DATABASE_PASSWORD'));
     console.log(this.configService.get<string>('database.user'));
     console.log(this.configService.get<string>('http.port')); //this is from our yaml config file
+
+    // we can get whole nested configuration object by using an interface as the type hint
+    const database = this.configService.get<DatabaseConfig>('database');
+    console.log(
+      'user and pass from interface type hint: ',
+      database.user,
+      database.pass,
+    );
+
     return this.cats;
   }
 }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,14 @@
-export const configurationFactory = () => ({
+export interface DatabaseConfig {
+  user: string;
+  pass: string;
+}
+
+export interface Config {
+  port: number;
+  database: DatabaseConfig;
+}
+
+export const configurationFactory = (): Config => ({
   port: parseInt(process.env.PORT) || 3000,
   database: {
     user: process.env.DATABASE_USER,
